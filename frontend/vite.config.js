d6fbc6246ee4095c58ac613d10f9029600fa3f59
adexcel171 <godwillokenyi@gmail.com>
2025-02-19 07:20:49 +0100

    ev

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,20 @@
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+// export default defineConfig({
+//
+//   build: {
+//     outDir: "dist", // Ensures build goes to dist
+//   },
+// });
+import { defineConfig } from "vite";
+import path from "path";
+
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   build: {
-    outDir: "dist", // Ensures build goes to dist
+    outDir: path.resolve(__dirname, "dist"), // Ensures correct build location
   },
 });
