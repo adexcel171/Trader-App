caa0d0e48a4ee9929e15101377150004d39589e0
adexcel171 <godwillokenyi@gmail.com>
2025-03-05 02:02:14 +0100

    update 2

--- frontend/src/app/store.jsx
+++ frontend/src/app/store.jsx
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { userApiSlice } from "../services/userApi";
 import { transactionApiSlice } from "../services/transactionApi";
+import { cryptoApi, useAddCryptoMutation } from "../services/cryptoApi";
 import authReducer from "../services/authSlice";
 
 export const store = configureStore({
@@ -9,10 +10,12 @@ export const store = configureStore({
     auth: authReducer,
     [userApiSlice.reducerPath]: userApiSlice.reducer,
     [transactionApiSlice.reducerPath]: transactionApiSlice.reducer,
+    [cryptoApi.reducerPath]: cryptoApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       userApiSlice.middleware,
+      cryptoApi.middleware,
       transactionApiSlice.middleware
     ),
 });
